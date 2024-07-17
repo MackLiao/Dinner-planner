67a6f0f2ac50573d1392e0061834871ad9e443be
Mack Liao <g.liao@wustl.edu>
2024-07-16 22:55:27 -0500

    Imported MUI as UI components

--- frontend-dinner-designer/src/App.js
+++ frontend-dinner-designer/src/App.js
@@ -4,6 +4,12 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 function App() {
   return (
     <Router>
